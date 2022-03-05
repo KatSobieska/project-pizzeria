bf4b6431e2af4795128cb28127aef83789642f47
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-03-05 18:52:21 +0100

    Add class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -291,6 +291,25 @@
 
     } 
   }
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
   const app = {
     initMenu: function(){
       const thisApp = this;
