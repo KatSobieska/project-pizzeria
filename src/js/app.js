9225de453720fb18452b28ed667299fbf18d6100
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-03-16 07:33:46 +0100

    Add Booking import path, add initBooking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { settings, select, classNames, templates } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
   initPages: function () {
@@ -93,6 +94,13 @@ const app = {
       app.cart.add(event.detail.product);
     });
   },
+  initBooking: function () {
+    const thisApp = this;
+
+    const pageReservation = document.querySelector(select.containerOf.booking);
+
+    thisApp.booking = new Booking(pageReservation);
+  },
   init: function () {
     const thisApp = this;
     console.log('*** App starting ***');
@@ -105,6 +113,7 @@ const app = {
 
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
