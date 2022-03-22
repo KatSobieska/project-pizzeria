a3327ef01a97c1be898595debdb30128f9f465a3
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-03-22 19:47:20 +0100

    Add new class Home

---
+++ src/js/components/Home.js
@@ -0,0 +1,28 @@
+import { templates, select } from '../settings.js';
+import utils from '../utils.js';
+
+class Home {
+  constructor(element) {
+    const thisHome = this;
+
+    thisHome.render(element);
+    thisHome.initWidgets();
+  }
+
+  render(element) {
+    const thisHome = this;
+
+    const generatedHTML = templates.bookingWidget();
+
+    thisHome.element = utils.createDOMFromHTML(generatedHTML);
+
+    const homeContainer = document.querySelector(select.containerOf.home);
+
+    homeContainer.appendChild(thisHome.element).innerHTML;
+
+    thisHome.dom = {};
+    thisHome.dom.wrapper = element;
+  }
+}
+
+export default Home;
